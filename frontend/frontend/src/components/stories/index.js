f7d05060fa020efcba73d8b71566f17430b07bae
hsnliqdrv <heseneli536@gmail.com>
2026-02-16 00:57:57 +0400

    move frontend folder

--- frontend/frontend/src/components/stories/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StoryCard } from "./StoryCard";
-export { default as ContentSelector } from "./ContentSelector";
-export { default as StoryCustomizer } from "./StoryCustomizer";
-export { default as StoryGenerator } from "./StoryGenerator";
